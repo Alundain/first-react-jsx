da1e6df25444d26b1cb67361abfd0ed84a008378
Dorian Donoso <adonosoaster@gmail.com>
2021-06-06 20:50:50 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello Dojo!</h1>
+            <p>Things I need to do</p>
+            <ul>
+                <li>Read react</li>
+                <li>Climb the everest</li>
+                <li>Run de marathon</li>
+                <li>Feed the dogs</li>
+            </ul>
     </div>
   );
 }
